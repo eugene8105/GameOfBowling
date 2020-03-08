fcf566db6b54a50c34b277b9cd3c9ca002ad27cc
[Eugene Bobalo] <eugenebobalo@gmail.com>
2020-03-08 12:05:28 -0500

    some changes

--- GameOfBowling/src/com/ebobalo/U2/ParkingApp/Frames.java
+++ GameOfBowling/src/com/ebobalo/U2/ParkingApp/Frames.java
@@ -4,19 +4,21 @@ import java.awt.*;
 
 public class Frames {
     private int roll_1;
-    public int getRoll_1() { return roll_1; }
-    public void setRoll_1(int roll_1) { this.roll_1 = roll_1; }
+    //public int getRoll_1() { return roll_1; }
+    //public void setRoll_1(int roll_1) { this.roll_1 = roll_1; }
     private int roll_2;
-    public int getRoll_2() { return roll_2; }
-    public void setRoll_2(int roll_2) { this.roll_2 = roll_2; }
+    //public int getRoll_2() { return roll_2; }
+    //public void setRoll_2(int roll_2) { this.roll_2 = roll_2; }
     private int roll_3;
-    public int getRoll_3() { return roll_3; }
-    public void setRoll_3(int roll_3) { this.roll_3 = roll_3; }
+    //public int getRoll_3() { return roll_3; }
+    //public void setRoll_3(int roll_3) { this.roll_3 = roll_3; }
     private int points;
-    public int getPoints() { return points; }
-    public void setPoints(int points) { this.points = points; }
+    //public int getPoints() { return points; }
+    //public void setPoints(int points) { this.points = points; }
 
-    public Frames(){
-
+    public Frames(int r1, int r2, int r3){
+        this.roll_1 = r1;
+        this.roll_2 = r2;
+        this.roll_3 = r3;
     }
 }
--- GameOfBowling/src/com/ebobalo/U2/ParkingApp/InputClass.java
+++ GameOfBowling/src/com/ebobalo/U2/ParkingApp/InputClass.java
@@ -1,36 +1,53 @@
 package com.ebobalo.U2.ParkingApp;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputClass {
 
     Scanner sc = new Scanner(System.in);
-    ArrayList<Frames> rl = new ArrayList<>();
-    int r_1=0;
-    int r_2=0;
-    int r_3=0;
-    int rollingNumber = 1;
+    List<Frames> frameList = new ArrayList<>();
+    int r_1 = 0;
+    int r_2 = 0;
+    int r_3 = 0;
+    int total = 0;
+    int rollingNumberOutput = 1;
 
     public InputClass(){
-        rollGameHelper();
+        for (int i = 0; i <= 10; i++){
+            rollGameHelper();
+        }
+
     }
 
     private void rollGameHelper(){
         System.out.flush();
         for (int i = 0; i <= 2; i++){
-            letsRoll();
-            rollingNumber++;
-            rollBoll();
+
         }
-
+        letsRoll();
+        rollingNumberOutput++;
+        setRollPoints(r_1);
+        setRollPoints(r_2);
+        setRollPoints(r_3);
+        populateFrame();
+        System.out.println();
+    }
+    private void printAllFrames(){
+        for (Frames element : frameList) {
+            System.out.println(element);
+        }
     }
 
-    private void rollBoll(){
-        CalcClass.rendNumGenerator(5);
+    private void setRollPoints(int rollNumber){
+        rollNumber = CalcClass.rendNumGenerator(5);
     }
-
+    private void populateFrame(){
+        Frames fm = new Frames(r_1,r_2,r_3);
+        frameList.add(fm);
+    }
     private void letsRoll(){
-        System.out.println(rollingNumber + " roll: ");
+        System.out.println(rollingNumberOutput + " roll: ");
     }
 }
